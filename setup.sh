b9fd1c9655dfa0471e9d72bb783773a071a2483e
Phil Weber <phil@philweber.com>
2024-03-27 21:44:00 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 echo
 echo Updating repositories...
-wget -O- https://apt.releases.hashicorp.com/gpg > /dev/null 2>&1 | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null 2>&1
+wget -q -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null 2>&1
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list > /dev/null 2>&1
 sudo apt update > /dev/null 2>&1
 sudo scripts/preinst.sh
